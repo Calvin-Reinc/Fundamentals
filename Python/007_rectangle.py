4e8446972ca36b202b0335941611c7e5947097d4
Calvin-Reinc <konyanacv@gmail.com>
2024-05-15 08:16:55 +0200

    This is oop. I learned about classes and using self. This programme prints out the rectangle with the data provided by the user and prints out the area of the shape.

---
+++ Python/007_rectangle.py
@@ -0,0 +1,31 @@
+# 5. Object-Oriented Programming
+# Task 8: Define a class named Rectangle that can be used to create 
+# rectangle objects. Include an initializer, properties for width and 
+# height, and a method to calculate the area of the rectangle. 
+# Create several rectangle objects, calculate their area, and 
+# print the results.
+class Rectangle:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        return self.width*self.height
+    
+    def rectangles(self):
+        for y in range(self.height): # controls columns
+            print("*"*self.width)
+            
+    def area(self):
+        area = int(self.height * (self.width))
+        print(f"The area of the rectangle is {str(area)}")
+            
+
+if __name__ == "__main__":
+    value1 = int(input("enter width: "))
+    value2 = int(input("enter height: "))
+    myRect = Rectangle(value1, value2)
+    myRect.rectangles()
+    myRect.area(    )
+
