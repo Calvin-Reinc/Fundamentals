11fc7621eb865dbf215742f48444d741b9582e92
Calvin-Reinc <konyanacv@gmail.com>
2024-05-16 15:56:37 +0200

    even and odds completed

--- JavaScript/003_odds.js
+++ JavaScript/003_odds.js
@@ -1,1 +1,15 @@
-dsfsdf
+// # Task 2: Write a program that asks the user to enter a number. 
+// # Check if the number is even or odd. Then, print an appropriate message to the user.
+
+function odds(number) {
+    if (number % 2 == 0) {
+        console.log("That's an even number.");
+    } else {
+        console.log("That's an odd number.");
+    }
+}
+
+const prompt = require("prompt-sync")({sigint:true});
+var number = prompt("Enter a number: ");
+number = Number(number);
+odds(number);
