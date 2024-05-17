5e33876a94d314e30ce6f9e481bd35400f517015
Calvin-Reinc <konyanacv@gmail.com>
2024-05-17 19:42:27 +0200

    phonebook console

---
+++ JavaScript/004_phoneBook.js
@@ -0,0 +1,73 @@
+const prompt = require("prompt-sync")({sigint:true});
+let phoneList = {}
+
+function add_phonebook() {
+    let myBool = true;
+    let userName = "";
+    let userNumber = "";
+    while (myBool) {
+        userName = prompt("Enter your name: ");
+        userNumber = prompt("Enter your phone number:");
+        phoneList[userName] = userNumber
+        myBool = false
+    }
+    return userName;
+}
+
+function delete_phone(userName) {
+    delete phoneList[userName];
+    return phoneList;
+}
+
+function search_phone() {
+    let lookFor = prompt("Whats the name of the contact: ");
+    console.log("searching...");
+    let result = phoneList.include(lookFor);
+    if (result) {
+        console.log(`${lookFor} - ${phoneList[lookFor]}`);
+    } else {
+        console.log("Contact not found.");
+    }
+}
+
+function view_phone() {
+    for (let key in phoneList) {
+        console.log(`${key} - ${phoneList[key]}`);
+    }
+}
+
+function main() {
+    let myBool = true;
+    let userName, userNumber
+    console.log("---- PhoneBook ----")
+    while (myBool) {
+        console.log("1. Add new contact"+"\n"+"2. Delete contact"+"\n"+"3. Search"+"\n"+"4. Display contacts"+"\n"+"Q to quit");
+        let work = (prompt("select option(Use numbers): "));
+        if (work.charAt(0) == "1") {
+            userName = add_phonebook();
+            console.log(`${userName}:  ${phoneList[userName]}\n Contact saved.`);
+        } else {
+            if (work.charAt(0) == "2") {
+                userName = prompt("Search by name: ");
+                delete_phone(userName);
+                console.log("Deleted!")
+            } else {
+                if (work.charAt(0) == "3") {
+                    search_phone();
+                } else {
+                    if (work.charAt(0).toUpperCase() == "Q") {
+                        myBool = false;
+                    } else {
+                        if (work.charAt(0) == "4") {
+                            view_phone()
+                        } else {
+                            console.log("Select a valid option.(Use number to identify)");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+main()
